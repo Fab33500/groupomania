274d66baff26a423bffaa358abbc72dc775b5faf
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-11 22:07:48 +0200

    creation du fichier db et connexion a la Bdd

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const morgan = require("morgan");
+
+require("./config/db");
 
 const app = express();
 
 // logs
 app.use(morgan("dev"));
+
+// debug mongoose
+console.log("-----------------debug mongoose-----------");
+mongoose.set("debug", true);
 
 app.use((req, res) => {
   res.json({ message: "Votre requête a bien été reçue !" });
---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+// import pour se connecter a la bd
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//   export
+module.exports = mongoose;
