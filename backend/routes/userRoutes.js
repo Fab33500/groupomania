fcb434bad153658f2e3de7c06850f7dc66a7b8be
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-11 22:28:26 +0200

    creation du routeur

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// importations
+const express = require("express");
+
+const userCtrl = require("../controllers/userCtrl");
+const authUserCtrl = require("../controllers/authUserCtrl.js");
+
+const router = express.Router();
+
+module.exports = router;
