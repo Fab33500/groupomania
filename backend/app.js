c34506bc61c5ee250a8dc7d9cede32cc517af2c8
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-15 18:46:06 +0200

    gestion des erreurs CORS

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,20 @@ app.use(
   })
 );
 
+// gestion errurs de CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // route user
 app.use("/api/user", userRoutes);
 
