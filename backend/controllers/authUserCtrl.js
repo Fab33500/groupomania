4ad738ec5abe8b20652212a9575ab14480ba3ff5
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-15 18:10:21 +0200

    gestion erreur signup du catch

--- backend/controllers/authUserCtrl.js
+++ backend/controllers/authUserCtrl.js
@@ -1,1 +1,21 @@
+const userModel = require("../models/userModel");
+const { signUpErrors } = require("../utils/errors");
 
+// -----------------------------------------
+
+// inscription user
+exports.signup = async (req, res) => {
+  const { pseudo, email, password } = req.body;
+  console.log("<<<<<<<<<<< --> ,", req.body);
+
+  try {
+    const user = await userModel.create({ pseudo, email, password });
+
+    res.status(201).json({
+      msg: ` Le compte ${user.pseudo}, a été créé avec succès, ID :${user._id}`,
+    });
+  } catch (err) {
+    const errors = signUpErrors(err);
+    res.status(200).send(errors);
+  }
+};
