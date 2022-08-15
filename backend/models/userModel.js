d7f2502a427da93e6ee76cd8146946b169345189
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-15 18:07:48 +0200

    email et password crypté

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
+
+//  variables d'environnement
+require("dotenv").config();
+
+//chiffre email
+const cryptoJs = require("crypto-js");
+
+// cryptage du mot de passe
+const bcrypt = require("bcrypt");
+
 // verification de la validité de l'email
 const { isEmail } = require("validator");
 
@@ -40,14 +50,15 @@ const userSchema = new mongoose.Schema(
     job: {
       type: String,
       minLength: 3,
-      maxlength: 30,
+      maxlength: 8,
     },
     bio: {
       type: String,
       max: 1024,
     },
     isAdmin: {
-      type: boolean,
+      type: Boolean,
+      default: false,
     },
     likes: {
       type: [String],
@@ -57,5 +68,18 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// crypter le mdp avant l'enregistrement dans la bdd
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// crypter l'email avant l'enregistrement dans la bdd
+userSchema.pre("save", async function (next) {
+  this.email = await cryptoJs
+    .HmacSHA256(this.email, process.env.CRYPTOJS_EMAIL)
+    .toString();
+});
 
 module.exports = mongoose.model("user", userSchema);
