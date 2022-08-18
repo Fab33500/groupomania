77c0eefe222b1b071a8eaa95eb148641c3f774b5
Fab33500 <lauretfabrice2@protonmail.com>
2022-08-18 16:46:25 +0200

    middleware de verification du proprietaire

---
+++ backend/middlewares/isOwner.js
@@ -0,0 +1,18 @@
+const userModel = require("../models/userModel");
+
+module.exports = async (req, res, next) => {
+  // controle l'utilisateur qui fait la requete à user ID
+  // si ce n'est pas le proprietaire:
+  if (req.params.id !== req.auth.userId) {
+    console.log("-----------req param", req.params.id);
+    console.log("req auth----------", req.auth.userId);
+    return res.status(401).json({
+      msg: " vous n'etes pas le proprietaire du compte!!",
+    });
+  }
+
+  // si la requete est faite par le propriétaire
+  else {
+    next();
+  }
+};
