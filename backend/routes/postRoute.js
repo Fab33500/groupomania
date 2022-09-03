ee1f15b67919c1c78f8288ef6eb9a6fc0504e76e
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-03 10:23:06 +0200

    création route lique et unlike

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -11,5 +11,7 @@ router.get("/", authorization, postCtrl.readPost);
 router.post("/", authorization, postCtrl.createPost);
 router.put("/:id", authorization, isPostOwner, postCtrl.updatePost);
 router.delete("/:id", authorization, isPostOwner, postCtrl.deletePost);
+router.patch("/like-post/:id", postCtrl.likePost);
+router.patch("/unlike-post/:id", postCtrl.unLikePost);
 
 module.exports = router;
