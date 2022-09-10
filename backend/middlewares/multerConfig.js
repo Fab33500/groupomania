041e672a401500ab9a72c5796b5fafd126c5aa49
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-10 18:51:56 +0200

    mise en place du middelware multer

---
+++ backend/middlewares/multerConfig.js
@@ -0,0 +1,47 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/gif": "gif",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "public/uploads/userAvatar");
+  },
+  filename: (req, file, callback) => {
+    const originalName = file.originalname.split(".")[0];
+    const name = originalName.split(" ").join("_") + "_";
+
+    // const name = file.fieldname + "_";
+    const extension = MIME_TYPES[file.mimetype];
+    callback(
+      null,
+      name +
+        Date.now() +
+        "_" +
+        Math.round(Math.random() * 1e9) +
+        "." +
+        extension
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype.includes("jpeg") ||
+    file.mimetype.includes("png") ||
+    file.mimetype.includes("jpg") ||
+    file.mimetype.includes("gif")
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false, "cc");
+  }
+};
+
+module.exports = multer({ storage, fileFilter }).single("avatar");
