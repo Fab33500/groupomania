9e6aa9e7fcd66118422a7e5700bcd075c48567cf
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-23 19:26:40 +0200

    modification des autorisations

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const postCtrl = require("../controllers/postCtrl");
 
-const authorization = require("../middlewares/auth");
+// const authorization = require("../middlewares/auth");
 const { isPostOwner } = require("../middlewares/isOwner");
 
 // post
-router.get("/", authorization, postCtrl.readPost);
-router.post("/", authorization, postCtrl.createPost);
-router.put("/:id", authorization, isPostOwner, postCtrl.updatePost);
-router.delete("/:id", authorization, isPostOwner, postCtrl.deletePost);
+router.get("/", postCtrl.readPost);
+router.post("/", postCtrl.createPost);
+router.put("/:id", postCtrl.updatePost);
+router.delete("/:id", postCtrl.deletePost);
 router.patch("/like-post/:id", postCtrl.likePost);
 router.patch("/unlike-post/:id", postCtrl.unLikePost);
 
