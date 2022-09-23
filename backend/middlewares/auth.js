f4b8a340d5ca330eaaf398881d3b719408865e1e
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-23 19:17:17 +0200

    creation d'un cookie de connexion

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,25 +1,49 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const userModel = require("../models/userModel");
 
-const { authErrors } = require("../utils/errors");
+// const { authErrors } = require("../utils/errors");
 
-module.exports = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
-    console.log(decodedToken.userId);
+// verifier si l'user est connecté tout au long de sa visite
 
-    const userId = decodedToken.userId;
-    req.auth = { userId };
-    console.log("<--------req.auth", req.auth);
+exports.checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
 
-    if (req.params.id && decodedToken.userId !== userId) {
-      throw err;
-    } else {
-      next();
-    }
-  } catch (err) {
-    const errors = authErrors(err);
-    res.status(401).json(errors);
+  if (token) {
+    jwt.verify(token, process.env.JWT_TOKEN, async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        res.cookie("jwt", "", { maxAge: 1 });
+        next();
+      } else {
+        let user = await userModel.findById(decodedToken.id);
+
+        // res.locals.user = user;
+        req.userToken = decodedToken;
+        console.log("user middlware auth ---------------", user);
+        next();
+      }
+    });
+  } else {
+    console.log("cookie null-----------------", token);
+    res.status(400).json({ msg: "Vous n'etes pas connecté" });
+  }
+};
+
+exports.requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_TOKEN, async (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+        res.send(400).json("pas de token");
+      } else {
+        console.log(decodedToken.id);
+        next();
+      }
+    });
+  } else {
+    console.log("pas de token");
   }
 };
