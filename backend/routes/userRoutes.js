d72f56058949085c7c2baf7faadd3ee1a47ef16d
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-23 19:27:50 +0200

    modification des autorisations

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,20 +5,21 @@ const password = require("../middlewares/passwordValidator");
 const authUserCtrl = require("../controllers/authUserCtrl.js");
 const userCtrl = require("../controllers/userCtrl");
 
-const authorization = require("../middlewares/auth");
+// const authorization = require("../middlewares/auth");
 const { isOwner } = require("../middlewares/isOwner");
+const { checkUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
 // AuthUser
 router.post("/signup", password, authUserCtrl.signup);
 router.post("/login", password, authUserCtrl.login);
-router.get("/logout", authUserCtrl.logout);
+router.get("/logout", checkUser, authUserCtrl.logout);
 
 // user
-router.get("/", authorization, userCtrl.getAllUsers);
-router.get("/:id", authorization, isOwner, userCtrl.getOneUser);
-router.put("/:id", authorization, isOwner, userCtrl.updateUser);
-router.delete("/:id", authorization, isOwner, userCtrl.deleteUser);
+router.get("/", checkUser, userCtrl.getAllUsers);
+router.get("/:id", checkUser, userCtrl.getOneUser);
+router.put("/:id", checkUser, isOwner, userCtrl.updateUser);
+router.delete("/:id", checkUser, isOwner, userCtrl.deleteUser);
 
 module.exports = router;
