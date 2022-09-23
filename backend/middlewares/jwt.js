7980cf31a24f42c8ad7e9a5660068c13f1a4675a
Fab33500 <lauretfabrice2@protonmail.com>
2022-09-23 19:20:50 +0200

    suppression du delais du token

--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -2,13 +2,8 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const JWT_TOKEN = process.env.JWT_TOKEN;
 
-// const user = userModel.login(emailCryptoJs, password);
-const maxAge = 24 * 60 * 60 * 1000; // expire dans 24h
-
-module.exports = {
-  generateTokenForUser: (user) => {
-    return jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, JWT_TOKEN, {
-      expiresIn: maxAge,
-    });
-  },
+const generateTokenForUser = (id, isAdmin) => {
+  return jwt.sign({ id, isAdmin }, JWT_TOKEN);
 };
+
+module.exports = generateTokenForUser;
