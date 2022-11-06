2a0c57ec644f19ae234ec405e7be864d64fed418
Fab33500 <lauretfabrice2@protonmail.com>
2022-11-06 17:32:03 +0100

    remplacement du dossier

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Groupomania</h1>
+      <i className="fa fa-solid fa-user"></i>
+      <i class="fa fa-regular fa-envelope"></i>
+    </div>
+  );
+}
+
+export default Header;
